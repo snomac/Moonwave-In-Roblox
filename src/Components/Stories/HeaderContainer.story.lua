eed4fe59121b7a30031ef237faa15e651b04ccb4
snomac <84568710+snomac@users.noreply.github.com>
2022-10-19 21:22:55 +0800

    Made HeaderContainer code more concise

--- src/Components/Stories/HeaderContainer.story.lua
+++ src/Components/Stories/HeaderContainer.story.lua
@@ -9,10 +9,12 @@ local Label = require(plugin.Components.Label)
 
 local TextScaled = require(plugin.Components.UIUtils.TextScaled)
 
+local sizes = {40, 33, 28}
+
 local function Header()
     return Label {
         -- just for testing's sake 😳
-        TextSize = ({40, 33, 28})[math.random(1, 3)],
+        TextSize = sizes[math.random(1, 3)],
         Text = "Header",
         [TextScaled] = Value(true)
     }
@@ -23,18 +25,9 @@ return function(target)
         Parent = target,
         Position = UDim2.fromScale(0.15, 0.15),
         [Children] = {
-            Header {
-                TextSize = 40,
-                Text = "Promise",
-            },
-            Header {
-                TextSize = 33,
-                Text = "Promise",
-            },
-            Header {
-                TextSize = 28,
-                Text = "Promise",
-            },
+            Header {},
+            Header {},
+            Header {},
         }
     }
 
