8ce874133dbd5aca45c612970ad155181a4d95f0
snomac <84568710+snomac@users.noreply.github.com>
2023-01-04 22:31:28 +0800

    Cleansed unnecessary code

--- src/Components/Label.lua
+++ src/Components/Label.lua
@@ -1,19 +1,14 @@
-local TextService = game:GetService("TextService")
-
 local plugin = script.Parent.Parent
 local Fusion = require(plugin.Packages.Fusion)
 
 local New = Fusion.New
-local Value = Fusion.Value
 local Hydrate = Fusion.Hydrate
-local Ref = Fusion.Ref
 
 type LabelProps = {
     [any]: any
 }
 
 local function Label(props : LabelProps)
-
     return Hydrate(New "TextLabel" {
         Size = UDim2.new(0, 15, 0, 50),
         AnchorPoint = Vector2.new(0, 0),
